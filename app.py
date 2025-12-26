f910a2a85d6c9d695a1c715fc4da75cfefbddf4d
golgetozu <golgetozu@gmail.com>
2025-12-26 15:20:23 +0300

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="Oto Sigorta Analiz", page_icon="🚗", layout="wide")
+st.set_page_config(page_title="InsureA Trafik Analiz", page_icon="🚗", layout="wide")
 
 import pandas as pd
 import numpy as np
